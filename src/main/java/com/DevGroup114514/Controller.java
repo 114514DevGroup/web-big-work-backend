f9c7ff19db9d6cda3a6df017da3c5b1453b8306a
Gayloc <gu18647403665@outlook.com>
2024-07-05 20:40:55 +0800

    httpserver基础完成

--- src/main/java/com/DevGroup114514/Controller.java
+++ src/main/java/com/DevGroup114514/Controller.java
@@ -6,10 +6,6 @@ package com.DevGroup114514;
  */
 
 public class Controller implements UserCount, UserManage, VideoManage {
-    @Override
-    public boolean put(User user) {
-        return false;
-    }
 
     @Override
     public boolean register(String username, String password) {
@@ -27,17 +23,8 @@ public class Controller implements UserCount, UserManage, VideoManage {
     }
 
     @Override
-    public boolean sendVideo(Video video) {
-        return false;
-    }
-
-    @Override
     public Video[] getRandomVideo() {
         return new Video[0];
     }
 
-    @Override
-    public boolean deleteVideo(int id) {
-        return false;
-    }
 }
